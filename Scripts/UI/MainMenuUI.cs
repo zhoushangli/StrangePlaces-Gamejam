3a16dfd02d43b25bac53fea86e6acac95cac5e51
superz63 <wangzhh1@shanghaitech.edu.cn>
2026-02-18 22:13:39 +0800

    [Wang Zhihang] 优化阴影 添加粒子效果 优化UI

--- Scripts/UI/MainMenuUI.cs
+++ Scripts/UI/MainMenuUI.cs
@@ -9,14 +9,41 @@ namespace Protogame2D.UI;
 /// </summary>
 public partial class MainMenuUI : UIBase
 {
-    [Export] private Button _startButton;
-    [Export] private Button _quitButton;
-
+    [Export] private TextureButton _startButton;
+    [Export] private TextureButton _quitButton;
+    [Export] private TextureButton _pressStartButton;
+    [Export] private NinePatchRect _StartPlate;
+    [Export] float sparkSpeed;
+    float increasing;
     public override void OnOpen(object args)
     {
+
+        increasing = 1.0f;
         _startButton.Pressed += OnStartPressed;
         _quitButton.Pressed += OnQuitPressed;
+        _pressStartButton.Pressed += OnPressStartPressed;
+        _StartPlate.Visible = false;
     }
+    public override void _Process(double delta)
+    {
+        base._Process(delta);
+        Spark(delta);
+    }
+    void Spark(double delta)
+    {
+        float oldAlpha = _pressStartButton.Modulate.A;
+        if(oldAlpha >= 1.0f)
+        {
+            increasing = -1.0f;
+        }
+        else if(oldAlpha <= 0.0f)
+        {
+            increasing = 1.0f;
+        }
+        oldAlpha += increasing * sparkSpeed * (float)delta;
+        _pressStartButton.Modulate = new Color(1,1,1,oldAlpha);
+    }
+
 
     public override void OnClose()
     {
@@ -25,11 +52,17 @@ public partial class MainMenuUI : UIBase
 
     private void OnStartPressed()
     {
+        Game.Instance.Get<UIService>().CloseTop();
         Game.Instance.Get<GameStateService>().ChangeGameState(GameState.Play);
     }
 
     private void OnQuitPressed()
     {
         GetTree().Quit();
+    }
+    private void OnPressStartPressed()
+    {
+        _pressStartButton.Visible = false;
+        _StartPlate.Visible=true;
     }
 }
---
+++ Scripts/UI/PauseUI.cs
@@ -0,0 +1,59 @@
+using Godot;
+using Protogame2D.Core;
+using Protogame2D.Services;
+
+namespace Protogame2D.UI;
+
+/// <summary>
+/// 暂停菜单，由 UIService.Open&lt;UI_Pause&gt; 打开。
+/// </summary>
+public partial class PauseUI : UIBase
+{
+    [Export]
+    private TextureButton _resumeButton;
+    [Export]
+    private TextureButton _backButton;
+    [Export]
+    private TextureButton _quitButton;
+
+    public override void _Ready()
+    {
+        // _resumeButton = GetNode<Button>("CenterContainer/VBoxContainer/ResumeButton");
+        // _backButton = GetNode<Button>("CenterContainer/VBoxContainer/BackToMenuButton");
+
+        // _resumeButton.Pressed += OnResumePressed;
+        // _backButton.Pressed += OnBackPressed;
+        // _quitButton.Pressed += OnQuitPressed;
+    }
+    
+    private void OnResumePressed()
+    {
+        // QueueFree();
+        Game.Instance.Get<UIService>().CloseTop();
+    }
+
+    private void OnBackPressed()
+    {
+        Game.Instance.Get<GameStateService>().ChangeGameState(GameState.MainMenu);
+    }
+    private void OnQuitPressed()
+    {
+        //关闭游戏
+        GetTree().Quit();
+    }
+
+    public override void OnOpen(object args)
+    {
+        //我把该函数功能理解为_Ready函数。_xxxButton在inspector中赋值
+
+        _resumeButton.Pressed += OnResumePressed;
+        _backButton.Pressed += OnBackPressed;
+        _quitButton.Pressed += OnQuitPressed;
+    }
+
+    public override void OnClose()
+    {
+        //TODO
+    }
+
+}
